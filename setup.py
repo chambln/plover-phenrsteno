b1a58b72277a0304bae9fe9c139f3c3a9f3d75e5
Gregory Chamberlain <15685804+contrum@users.noreply.github.com>
2018-08-05 03:40:27 +0100

    Upload benign 0.0.12.1 to test whether Unicode is breaking the plugins manager

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with codecs.open('README.md', 'r', 'utf_8') as readme:
 
 setuptools.setup(
     name = 'plover-phenrsteno',
-    version = '0.0.13',
+    version = '0.0.12.1',
     author = 'Gregory Chamberlain',
     author_email = '15685804+contrum@users.noreply.github.com',
     description = 'Phonemic non-rhotic English stenotype system for Plover',
