d226a8c56fa9c412a7789097a1af3c06af0414a4
Gregory Chamberlain <15685804+contrum@users.noreply.github.com>
2018-08-06 22:36:04 +0100

    Change long_descriptiong='foo'

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name = 'plover-phenrsteno',
-    version = '0.1.7dev5',
+    version = '0.1.7dev6',
     author = 'Gregory Chamberlain',
     author_email = '15685804+contrum@users.noreply.github.com',
     description = 'Phonemic non-rhotic English stenotype system for Plover',
-    long_description = long_description,
+    long_description = "foo",
     long_description_content_type = 'text/x-rst',
     license = 'GNU General Public License v2 or later (GPLv2+)',
     keywords = 'plover plover_plugin',
