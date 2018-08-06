eada45a2ced043c4922d864e7d448a74d63129bf
Gregory Chamberlain <15685804+contrum@users.noreply.github.com>
2018-08-06 16:55:07 +0100

    Use os.path; add README.md as long_desccription

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
-import setuptools
-import codecs
+from setuptools import setup
+from codecs import open
+from os import path
 
-setuptools.setup(
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from README.md --> rst
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
     name = 'plover-phenrsteno',
     version = '0.1.7',
     author = 'Gregory Chamberlain',
     author_email = '15685804+contrum@users.noreply.github.com',
     description = 'Phonemic non-rhotic English stenotype system for Plover',
+    long_description = long_description,
     license = 'GNU General Public License v2 or later (GPLv2+)',
-    long_description = "",
     long_description_content_type = 'text/markdown',
     keywords = 'plover plover_plugin',
     url = 'https://github.com/contrum/phenrsteno',
