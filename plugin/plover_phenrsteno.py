3ea628cd5ad718be57c67da6e57937cb5d445339
Gregory Chamberlain <greg@cosine.blue>
2020-10-22 12:49:57 +0100

    Add licence header comment

--- plugin/plover_phenrsteno.py
+++ plugin/plover_phenrsteno.py
@@ -1,4 +1,19 @@
-# phenrsteno
+#    Phenrsteno - Phonemic steno theory for non-rhotic dialects
+#    Copyright (C) 2018  Gregory Chamberlain <greg@cosine.blue>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #   | ə p t w     f t S l i |  ~>  | # X p t w     f t S l i |
 #   | s b d r  *  n d g z ə |  ~>  | # s b d r  *  n d g z x |
